bab6d707fbe711f1896c14356ad1116be55386ed
Mithun S Menon <mithunsmenon@gmail.com>
2020-05-25 23:20:43 +0530

    Update app_v3_en.js

--- app_v3_en.js
+++ app_v3_en.js
@@ -290,15 +290,15 @@ function render_search_result_list(){var content=`
 	  <ul class="mdui-list"> 
 	   <li class="mdui-list-item th"> 
 	    <div class="mdui-col-xs-12 mdui-col-sm-7">
-	     ??
+	     File
 	<i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
 	    </div> 
 	    <div class="mdui-col-sm-3 mdui-text-right">
-	     ????
+	     Date Modified
 	<i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i>
 	    </div> 
 	    <div class="mdui-col-sm-2 mdui-text-right">
-	     ??
+	     Size
 	<i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i>
 	    </div> 
 	    </li> 
